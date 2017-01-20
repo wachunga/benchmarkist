df90df5368cdf83ac8137406fadfbd69be3c03f0
David Hirtle <david.hirtle@yardi.com>
2017-01-19 19:15:46 -0800

    add calculateTotalBalance functionality
    some tests are failing due to floating point math, which
    I'll fix

--- test/transactionServiceTest.js
+++ test/transactionServiceTest.js
@@ -5,11 +5,11 @@ const nock = require('nock');
 const transactionService = require('../lib/transactionService');
 
 describe('transactionService', () => {
-	function mockTransaction() {
+	function mockTransaction(amount) {
 		return {
 			Date: '2017-01-01',
 			Ledger: 'Business Meals & Entertainment Expense',
-			Amount: '-123.45',
+			Amount: amount || '-123.45',
 			Company: 'WHOLE FOODS'
 		};
 	}
@@ -17,59 +17,151 @@ describe('transactionService', () => {
 		return new Array(count).fill(mockTransaction());
 	}
 
-	it('downloads single page of transactions', () => {
-		nock(/.+/)
-			.get('/transactions/1.json')
-			.reply(200, {
-				totalCount: 2,
-				transactions: mockTransactions(2)
-			});
+	describe('downloadAllTransactions', () => {
+		it('downloads single page of transactions', () => {
+			nock(/.+/)
+				.get('/transactions/1.json')
+				.reply(200, {
+					totalCount: 2,
+					transactions: mockTransactions(2)
+				});
 
-		return transactionService.downloadAllTransactions().then(transactions => {
-			assert.equal(transactions.length, 2);
+			return transactionService.downloadAllTransactions().then(transactions => {
+				assert.equal(transactions.length, 2);
+			});
+		});
+
+		it('downloads multiple pages of transactions', () => {
+			nock(/.+/)
+				.get('/transactions/1.json')
+				.reply(200, {
+					totalCount: 3,
+					transactions: mockTransactions(2)
+				})
+				.get('/transactions/2.json')
+				.reply(200, {
+					totalCount: 3,
+					transactions: mockTransactions(1)
+				});
+
+			return transactionService.downloadAllTransactions().then(transactions => {
+				assert.equal(transactions.length, 3);
+			});
+		});
+
+		it('includes status code in propagated error', () => {
+			nock(/.+/)
+				.get('/transactions/1.json')
+				.reply(500);
+
+			return transactionService.downloadAllTransactions().then(() => {
+				assert.fail(null, null, 'should not be a success');
+			}).catch(err => {
+				assert.equal(err.statusCode, 500);
+			});
+		});
+
+		it('handles no transactions', () => {
+			nock(/.+/)
+				.get('/transactions/1.json')
+				.reply(200, {
+					totalCount: 0,
+					transactions: []
+				});
+
+			return transactionService.downloadAllTransactions().then(transactions => {
+				assert.equal(transactions.length, 0);
+			});
 		});
 	});
 
-	it('downloads multiple pages of transactions', () => {
-		nock(/.+/)
-			.get('/transactions/1.json')
-			.reply(200, {
-				totalCount: 3,
-				transactions: mockTransactions(2)
-			})
-			.get('/transactions/2.json')
-			.reply(200, {
-				totalCount: 3,
-				transactions: mockTransactions(1)
+	describe('calculateTotalBalance', () => {
+		it('sums round dollars', () => {
+			nock(/.+/)
+				.get('/transactions/1.json')
+				.reply(200, {
+					totalCount: 2,
+					transactions: [
+						mockTransaction('100'),
+						mockTransaction('50'),
+					]
+				});
+
+			return transactionService.calculateTotalBalance().then(balance => {
+				assert.equal(balance, '150');
 			});
-
-		return transactionService.downloadAllTransactions().then(transactions => {
-			assert.equal(transactions.length, 3);
 		});
-	});
 
-	it('includes status code in propagated error', () => {
-		nock(/.+/)
-			.get('/transactions/1.json')
-			.reply(500);
+		it('sums dollars and cents', () => {
+			nock(/.+/)
+				.get('/transactions/1.json')
+				.reply(200, {
+					totalCount: 2,
+					transactions: [
+						mockTransaction('100.10'),
+						mockTransaction('50.40'),
+					]
+				});
 
-		return transactionService.downloadAllTransactions().then(() => {
-			assert.fail(null, null, 'should not be a success');
-		}).catch(err => {
-			assert.equal(err.statusCode, 500);
-		});
-	});
-
-	it('handles no transactions', () => {
-		nock(/.+/)
-			.get('/transactions/1.json')
-			.reply(200, {
-				totalCount: 0,
-				transactions: []
+			return transactionService.calculateTotalBalance().then(balance => {
+				assert.equal(balance, '150.50');
 			});
+		});
 
-		return transactionService.downloadAllTransactions().then(transactions => {
-			assert.equal(transactions.length, 0);
+		it('sums negative amounts', () => {
+			nock(/.+/)
+				.get('/transactions/1.json')
+				.reply(200, {
+					totalCount: 2,
+					transactions: [
+						mockTransaction('-100.50'),
+						mockTransaction('50.50'),
+					]
+				});
+
+			return transactionService.calculateTotalBalance().then(balance => {
+				assert.equal(balance, '-50');
+			});
+		});
+
+		it('sums multiple pages', () => {
+			nock(/.+/)
+				.get('/transactions/1.json')
+				.reply(200, {
+					totalCount: 4,
+					transactions: [
+						mockTransaction('100.10'),
+						mockTransaction('50.40'),
+					]
+				})
+				.get('/transactions/2.json')
+				.reply(200, {
+					totalCount: 4,
+					transactions: [
+						mockTransaction('-20.20'),
+						mockTransaction('-30.30'),
+					]
+				});
+
+			return transactionService.calculateTotalBalance().then(balance => {
+				assert.equal(balance, '100');
+			});
+		});
+
+		it('handles common floating point issues', () => {
+			nock(/.+/)
+				.get('/transactions/1.json')
+				.reply(200, {
+					totalCount: 2,
+					transactions: [
+						mockTransaction('0.1'),
+						mockTransaction('0.2'),
+					]
+				});
+
+			return transactionService.calculateTotalBalance().then(balance => {
+				assert.equal(balance, '0.3');
+			});
 		});
 	});
 });
