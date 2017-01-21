360423ddbff65a137157095d9531b5466a3942fe
David Hirtle <david.hirtle@yardi.com>
2017-01-21 08:16:22 -0800

    add basic server that serves up nothing... yet

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 'use strict';
 
-const transactionService = require('./lib/transactionService');
-
-transactionService.downloadAllTransactions().then(transactions => {
-	console.log(transactions);
-}).catch(err => {
-	console.log(err);
-});
-
-module.exports = transactionService;
+module.exports = require('./lib/app');
