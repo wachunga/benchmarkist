2b8050ce23b1bc4b6d175666bc4dc28222d02c44
David Hirtle <david.hirtle@yardi.com>
2017-01-24 09:37:56 -0800

    add visual indictor to suspiciously high expenses

--- app/js/expenses.js
+++ app/js/expenses.js
@@ -17,13 +17,28 @@
 		expensesSummaryElement.innerHTML = results.map(buildExpenseEntry).join('\n');
 	}
 
+	const flagIcon = '&#x1f914; '; // thinking face
+	const flagMessage = 'This expense is much higher than the benchmark. Why is that?';
+
 	function buildExpenseEntry(expense) {
 		const detailsTableRows = expense.transactions.map(transaction => {
 			return `<tr><td>${transaction.Date}</td><td>${transaction.Company}</td><td>${transaction.Amount}</td></tr>`;
 		}).join('\n');
 		const detailsTable = `<table class="expense-details-table"><tbody>${detailsTableRows}</tbody></table>`;
 		const detailsElement = `<details><summary>${expense.categoryKey}</summary>${detailsTable}</details>`;
+
 		const categoryId = `category-${expense.categoryKey.toLowerCase().split(/\W/)[0]}`;
-		return `<tr id="${categoryId}"><td>${detailsElement}</td><td>${expense.total}</td><td>${expense.percent}</td><td>${expense.benchmark}</td></tr>`;
+		const maybeFlaggedPercent = seemsHigh(expense) ?
+			`<span class="help" title="${flagMessage}">${flagIcon} ${expense.percent}</span>` :
+			expense.percent;
+		return `<tr id="${categoryId}"><td>${detailsElement}</td><td>${expense.total}</td><td>${maybeFlaggedPercent}</td><td>${expense.benchmark}</td></tr>`;
+	}
+
+	function seemsHigh(expense) {
+		const percent = parseFloat(expense.percent);
+		const benchmark = parseFloat(expense.benchmark);
+		const absoluteDiff = percent - benchmark;
+		const percentDiff = absoluteDiff / benchmark;
+		return absoluteDiff >= 2 && percentDiff > 0.2;
 	}
 }());
